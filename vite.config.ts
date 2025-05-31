3ed1589914c90db68f322892ba6b0462610c67f9
ebenezeraladesuyi <ebenezeraladesuyi@gmail.com>
2025-05-31 08:36:37 +0100

    pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,47 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react'
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA(
+      { 
+        registerType: "autoUpdate",
+        manifest: {
+        name: 'Volatic',
+        short_name: 'Volatic',
+        description: 'Where learning meets innovation',
+        theme_color: '#ffffff',
+        icons: [
+                {
+                  "src": "/pwa-192x192.png",
+                  "sizes": "192x192",
+                  "type": "image/png",
+                  "purpose": "any"
+                },
+                {
+                  "src": "/pwa-512x512.png",
+                  "sizes": "512x512",
+                  "type": "image/png",
+                  "purpose": "any"
+                },
+                {
+                  "src": "/pwa-maskable-192x192.png",
+                  "sizes": "192x192",
+                  "type": "image/png",
+                  "purpose": "maskable"
+                },
+                {
+                  "src": "/pwa-maskable-512x512.png",
+                  "sizes": "512x512",
+                  "type": "image/png",
+                  "purpose": "maskable"
+                }
+        ]
+      }
+      }
+    )
+  ],
 })
